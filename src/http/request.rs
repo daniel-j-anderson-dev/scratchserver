9ae2245af755d4fcd7cf19344403c94e69ec21be
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-04 23:09:26 -0500

    cargo fmt

--- src/http/request.rs
+++ src/http/request.rs
@@ -10,12 +10,20 @@ pub struct Request {
 impl FromStr for Request {
     type Err = Box<dyn std::error::Error>;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let verb = match s.split_whitespace().filter_map(|word| word.parse::<Verb>().ok()).next() {
+        let verb = match s
+            .split_whitespace()
+            .filter_map(|word| word.parse::<Verb>().ok())
+            .next()
+        {
             Some(verb) => verb,
             None => Err("No http Verb found in request")?,
         };
 
-        let url = match s.split_whitespace().filter_map(|word| word.parse::<Url>().ok()).next() {
+        let url = match s
+            .split_whitespace()
+            .filter_map(|word| word.parse::<Url>().ok())
+            .next()
+        {
             Some(url) => url,
             None => Err("Url could not be parsed")?,
         };
