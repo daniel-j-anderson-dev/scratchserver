9f7eab5a313b219589186ea608fa8e994df8fe9d
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-06 16:15:37 -0500

    Move connection handling logic into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,39 @@
-use std::{io::{BufReader, BufWriter}, net::TcpStream};
+pub mod my_http;
+use crate::my_http::{Request, Response};
 
-pub mod http;
+use color_eyre::{owo_colors::OwoColorize, Report};
+use std::{
+    io::{BufReader, BufWriter, Write},
+    net::TcpStream,
+};
+
+/// This assumes that the `web` directory lives in the root of the crate. and `cargo run` must be ran from the crate root
+pub const SERVE_DIR: &'static str = "./web";
+
+pub fn handle_connection(
+    (reader, mut writer): (BufReader<TcpStream>, BufWriter<TcpStream>),
+) -> Result<(), Report> {
+    println!(
+        "{}\n{}{:?}",
+        "Connection established".green(),
+        "Peer Address: ".green(),
+        reader.get_ref().peer_addr().green()
+    );
+
+    let request = Request::try_from(reader)?;
+    println!("{:#?}\n", request.bright_blue());
+
+    let response = Response::try_from(request)?;
+    if response.body_length() <= 1000 {
+        println!("response:\n{}", response.on_blue());
+    } else {
+        println!("response:\n{}\n{}\n{}", response.start_line().on_blue(), response.headers().on_blue(), "body omitted for brevity".on_blue());
+    }
+
+    writer.write_all(response.to_string().as_bytes())?;
+
+    return Ok(());
+}
 
 /// This function is meant to be used as the `predicate` for a [Iterator::filter_map] on [std::net::TcpListener::incoming]
 /// # Example
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,23 @@
-use scratchserver::{http::{Request, Response}, split_stream};
+use scratchserver::{handle_connection, split_stream};
 
-use std::{
-    io::BufRead,
-    net::TcpListener,
-};
+use color_eyre::{owo_colors::OwoColorize, Report};
+use std::{env, net::TcpListener};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ip = std::env::args().nth(1).unwrap_or("localhost:8000".to_string());
+fn main() -> Result<(), Report> {
+    color_eyre::install()?;
+
+    let ip = env::args().nth(1).unwrap_or("localhost:8000".to_string());
     let listener = TcpListener::bind(&ip)?;
 
     println!("\n------| Listening on http://localhost:8000 |------");
 
-    for (mut reader, writer) in listener.incoming().filter_map(split_stream) {
-        println!(
-            "Connection established\nPeer Address: {:?}",
-            reader.get_ref().peer_addr()
-        );
+    for connection in listener.incoming().filter_map(split_stream) {
+        if let Err(error) = handle_connection(connection) {
+            println!("Error handling connection: {}", error.on_red());
+        }
 
-        let mut raw_request = String::new();
-        match reader.read_line(&mut raw_request) {
-            Ok(_bytes_read) => (),
-            Err(error) => {
-                dbg!(raw_request);
-                println!("Failed to read from client; {}", error);
-                continue;
-            }
-        };
-
-        let request = match raw_request.parse::<Request>() {
-            Ok(request) => request,
-            Err(error) => {
-                println!("Failed to parse request\n{}\n", error);
-                continue;
-            }
-        };
-
-        println!("{:?}\nraw: {}", request, raw_request);
+        println!();
     }
 
     return Ok(());
 }
-
